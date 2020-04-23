43a10ee39ddf863a899505d35abb06c5d11e9f68
Alexander Stein <alexander.stein@gsa.gov>
2020-04-23 00:02:46 -0400

    Simple app construction.

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+from flask import jsonify, make_response, request, Flask
+import json
 import os
-from flask import Flask
+import urllib3
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello World from Flask!'
+@app.route('/stats', methods=["GET", "POST", "PUT"])
+def stats():
+    try:
+        pool_manager = urllib3.PoolManager()
+        method = request.method.upper() if request.method else 'GET'
+        host = request.args.get('host', default = "10x.gsa.gov", type = str)
+        url = f"https://{host}:443"
+        info = pool_manager.request(method, url).info()
+        return make_response(jsonify(data=dict(info)))
+    except Exception as err:
+        return make_response(jsonify(error=str(err)), 400)
 
 
 port = os.getenv('PORT', '5000')
