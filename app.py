f7bafcfa315206b4801be33ffbdde5ad747767e2
Alexander Stein <61464190+ohsh6o@users.noreply.github.com>
2020-04-28 00:11:51 -0400

    Tasks add exploitable code (#2)
    * Simple app construction.
    
    * MVP version of app allowing CRLF injections.

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
+from flask import jsonify, make_response, request, Flask
+import json
 import os
-from flask import Flask
+import urllib3
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello World from Flask!'
+@app.route('/stats', methods=["GET", "POST", "PUT"])
+def stats():
+    try:
+        resp = {}
+        pool_manager = urllib3.PoolManager()
+        method = request.method.upper() if request.method else 'GET'
+        host = request.args.get('host', default = "10x.gsa.gov", type = str)
+        url = f"https://{host}:443"
+        resp['result'] = dict(pool_manager.request(method, url).info())
+        return make_response(jsonify(**resp))
+    except Exception as err:
+        return make_response(jsonify(error=str(err)), 400)
 
 
 port = os.getenv('PORT', '5000')
