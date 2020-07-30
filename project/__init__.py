dbfc3014d26a12c9fbd2a1ffabe56058b106d278
Alexander Stein <astein@flexion.us>
2020-07-30 00:13:05 -0400

    String up auth DB and model stuff.

--- project/__init__.py
+++ project/__init__.py
@@ -1,19 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.get('SECRET_KEY', 'LOCALDEVONLY')
-    app.config['DB_URI'] = 'sqlite:///db.sqlite'
+    app.config['SECRET_KEY'] = environ.get('SECRET_KEY', 'LOCALDEVONLY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+    db = SQLAlchemy()
     db.init_app(app)
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+    with app.app_context():
+        from .auth import auth as auth_blueprint
+        app.register_blueprint(auth_blueprint)
 
-    from .main import  main as main_blueprint
-    app.register_blueprint(main_blueprint)
+        from .main import  main as main_blueprint
+        app.register_blueprint(main_blueprint)
 
-    return app
+        db.create_all()
+
+        return app
--- project/auth.py
+++ project/auth.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
-#from . import db
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -10,6 +12,33 @@ def login():
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        flash('Email address already exists')
+        return redirect(url_for('auth.signup'))
+
+    new_user = User(email=email,
+                    name=name, 
+                    password=generate_password_hash(
+                            password,
+                            method='pbkdf2:sha256',
+                            salt_length=8
+                        )
+                    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
+
 
 @auth.route('/logout')
 def logout():
