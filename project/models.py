88281ed33da551c5fb0672939f01cd5c8900a731
Alexander Stein <astein@flexion.us>
2020-08-04 00:14:22 -0400

    Stub out public & sensitive DB models.

--- project/models.py
+++ project/models.py
@@ -6,3 +6,13 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+class PublicItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(100))
+    value = db.Column(db.String(100))
+
+class SensitiveItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(100))
+    value = db.Column(db.String(100))
