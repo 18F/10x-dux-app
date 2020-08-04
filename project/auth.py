13a0c99574f505ddb63d0378aecba2d76b07642d
Alexander Stein <astein@flexion.us>
2020-08-03 20:58:47 -0400

    Flesh out login components and add UserMixin.

--- project/auth.py
+++ project/auth.py
@@ -9,6 +9,20 @@ auth = Blueprint('auth', __name__)
 def login():
     return render_template('login.html')
 
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login'))
+
+    return redirect(url_for('main.profile'))
+
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
