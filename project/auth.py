3e9fb896886312209213766c0d0d59818d5f3058
Alexander Stein <astein@flexion.us>
2020-08-03 21:47:54 -0400

    Clear session and logout user on logout.

--- project/auth.py
+++ project/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from flask_login import login_user
+from flask_login import login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
@@ -58,4 +58,5 @@ def signup_post():
 
 @auth.route('/logout')
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('main.index'))
