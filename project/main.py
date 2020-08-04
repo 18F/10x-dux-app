bc5fd3543c3a952bb7c48dd1424315e19e16322e
Alexander Stein <astein@flexion.us>
2020-08-04 00:14:05 -0400

    Build out public-private dichotomy.

--- project/main.py
+++ project/main.py
@@ -8,8 +8,16 @@ main = Blueprint('main', __name__, static_folder='static', template_folder='temp
 def index():
     return render_template('index.html')
 
+@main.route('/public')
+def public():
+    return render_template('public.html')
+
+@main.route('/sensitive')
+@login_required
+def sensitive():
+    return render_template('sensitive.html')
+
 @main.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
-
