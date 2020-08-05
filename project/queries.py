f71aa2be6dd823c87bf099b58f660c6271b3f853
Alexander Stein <astein@flexion.us>
2020-08-05 00:01:27 -0400

    Build group_by filtering for exploit.

--- project/queries.py
+++ project/queries.py
@@ -1,7 +1,22 @@
+from . import db
 from .models import PublicItem, SensitiveItem
 
-def get_public_items():
+def get_public_items(grouping=None):
+    if  grouping:
+        return PublicItem.query().grouping(grouping).all()
+        
     return PublicItem.query.all()
 
-def get_sensitve_items():
-    return SensitiveItem.query.all()    
+def get_public_items_groupings():
+    model = PublicItem
+    return [m.key for m in model.__table__.columns]
+
+def get_sensitve_items(grouping=None):
+    if  grouping:
+        return SensitiveItem.query().grouping(grouping).all()
+
+    return SensitiveItem.query.all()
+
+def get_sensitive_items_groupings():
+    model = SensitiveItem
+    return [m.key for m in model.__table__.columns]
